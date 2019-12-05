cce146660679e409f7847ff73dece9a6a2f681c0
Ludvig Hult <ludvig.hult@gmail.com>
2019-12-05 08:50:28 +0100

    Generator-obsessive Day4

---
+++ day4_lib.py
@@ -0,0 +1,46 @@
+import operator
+from itertools import tee, starmap, groupby
+from typing import Literal
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def has_six_digits(i: int):
+    return 100_000 <= i <= 999_999
+
+
+def is_nondecreasing_sequence(i: int):
+    pairs = pairwise(str(i))
+    goes_up = starmap(operator.le, pairs)
+    nondecreasing = all(goes_up)
+    return nondecreasing
+
+
+def one_pair_is_not_triplet(i: int):
+    return 2 in {sum(1 for _ in g) for _, g in groupby(str(i))}
+
+
+def has_pair(i: int):
+    pairs = pairwise(str(i))
+    is_equal = starmap(operator.eq, pairs)
+    return any(is_equal)
+
+
+def moar_numbers(start_inclusive, stop_inclusive, part=Literal['a', 'b']):
+    current = start_inclusive
+    check_extra = part == 'b'
+    while current <= stop_inclusive:
+        current_fulfils_spec = (
+                has_six_digits(current) and
+                is_nondecreasing_sequence(current) and
+                has_pair(current) and
+                (not check_extra or one_pair_is_not_triplet(current))
+        )
+        if current_fulfils_spec:
+            yield current
+        current += 1
