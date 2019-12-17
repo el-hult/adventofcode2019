b381e8332ad0079b17bdc8cfd9a6b37c7926865b
Ludvig Hult <ludvig.hult@gmail.com>
2019-12-17 18:28:50 +0100

    bugfix

--- day2_lib.py
+++ day2_lib.py
@@ -5,6 +5,6 @@ def compute_computer_output(program: Program, noun: int, verb: int) -> int:
     initial_state = program.copy()
     initial_state[1] = noun
     initial_state[2] = verb
-    cmp = Computer(initial_state)
+    cmp = Computer(initial_state,None,None)
     cmp.run_until_stop()
     return cmp._state[0]
